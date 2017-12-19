f05f1a35920690ce98fc8bd387618589842a4ab8
HotBlueStreak <HotBlueStreak@mail.ru>
2017-12-19 21:21:15 +0300

    Lee algorithm Alpha 2.0

--- Sources/Algorithms/LeeAlgorithm.cpp
+++ Sources/Algorithms/LeeAlgorithm.cpp
@@ -4,58 +4,58 @@ LeeAlgorithm LeeAlgorithm::algorithm_;
 
 void LeeAlgorithm::FindPath(sf::Vector2f start_position, sf::Vector2f end_position, short number_of_directions)
 {
-	// Инициализация
+	// Initialization
 	algorithm_ = LeeAlgorithm();
 	Map* map = Map::GetMap();
 
-	// Начальна/конечная позиция - стена, невозможно определить путь
+	// Start/end tile - wall, impossible to determine the path
 	if (map->GetTile(map->GetStart())->GetType() == TileType::Wall ||
 		map->GetTile(map->GetEnd())->GetType() == TileType::Wall) return;
 
-	// Количество направлений
+	// Number of directions
 	algorithm_.SetDirectionCount(number_of_directions);
 
 	bool open_tiles;
 	short current_distance = 0;
 
-	// Сбросить предыдущий результат
+	// Reset previous result
 	LeeAlgorithm::ResetPathMap();
 
-	// Стартовая ячейка помечена 0
+	// Start tile is marked 0
 	algorithm_.tile_distace_.at(map->GetStart().x).at(map->GetStart().y) = current_distance;
 
-	// Распространение волны
+	// Wave expansion
 	do {
-		open_tiles = false;		 // Предполагаем, что все свободные клетки уже проверены
+		open_tiles = false;		 // We assume that all free tiles have already been tested
 
 		for (auto y = 0; y < map->GetHeight(); ++y)
 			for (auto x = 0; x < map->GetWidth(); ++x)
 			{
-				if (algorithm_.tile_distace_.at(x).at(y) == current_distance)		// Ячейка помечена current_distance
+				if (algorithm_.tile_distace_.at(x).at(y) == current_distance)		// Tile is marked current_distance
 				{
-					// Отмечаем всех непомеченых соседей(в n направлениях) - current_distance + 1
+					// Note all unlabelled neighbors (in n directions) - current_distance + 1
 
 					for (auto i = 0; i < algorithm_.GetNumberOfDirections(); ++i)
 					{
-						// Находим координаты соседних ячеек(в n направлениях)
+						// Find the coordinates of neighboring tiles (in n directions)
 						short delta_y = y + algorithm_.offset_by_y_.at(i),
 							  delta_x = x + algorithm_.offset_by_x_.at(i);
 
 						if (delta_y >= 0 && delta_x >= 0 && delta_y < map->GetHeight() && delta_x < map->GetWidth()
 							&& algorithm_.tile_distace_.at(delta_x).at(delta_y) == -1)
 						{
-							open_tiles = true;												// Найдены непомеченые ячейки
-							algorithm_.checked_tile_.at(delta_x).at(delta_y) = true;		// Сохраняем помеченые ячейки
+							open_tiles = true;												// Unmarked tiles found
+							algorithm_.checked_tile_.at(delta_x).at(delta_y) = true;		// Save tagged tiles
 							algorithm_.tile_distace_.at(delta_x).at(delta_y) = current_distance + 1;
 							
 						}
 					}
 				}
 			}
-		current_distance++;			// Распространение волны
+		current_distance++;			// Propagation of the wave
 	} while (open_tiles && algorithm_.tile_distace_.at(map->GetEnd().x).at(map->GetEnd().y) == -1);
 
-	if (algorithm_.tile_distace_.at(map->GetEnd().x).at(map->GetEnd().y) == -1) return;			// Путь не найден
+	if (algorithm_.tile_distace_.at(map->GetEnd().x).at(map->GetEnd().y) == -1) return;			// Path not found
 
 	SetBackTrace();			
 	ShowCheckedTiles();		
@@ -67,38 +67,38 @@ void LeeAlgorithm::SetBackTrace()
 {
 	Map* map = Map::GetMap();
 
-	// Длина кратчайшего пути из начальной ячейки в конечную
+	// The length of the shortest path from the start tile to the final
 	algorithm_.SetPathLength(algorithm_.tile_distace_.at(map->GetEnd().x).at(map->GetEnd().y));
 
-	// Текущие координаты - координаты конечной ячейки
+	// Current coordinates - coordinates of the end tile
 	sf::Vector2f current_tile_position = map->GetEnd();
 
 	short current_distance = algorithm_.GetPathLength();
 
-	// Ищем путь, начиная с конечной ячейки 
+	// Looking for a path starting at the end tile
 	while (current_distance > 0)
 	{
-		// Сохраняем текущие координаты в путь
+		// Save the current coordinates in the path
 		algorithm_.path_map_.push_back(current_tile_position);
 		current_distance--;
 
 		for (auto i = 0; i < algorithm_.GetNumberOfDirections(); ++i)
 		{
-			// Находим координаты соседних ячеек(в n направлениях)
+			// Find the coordinates of neighboring tiles (in n directions)
 			short delta_y = current_tile_position.y + algorithm_.offset_by_y_.at(i),
 				  delta_x = current_tile_position.x + algorithm_.offset_by_x_.at(i);
 
 			if (delta_y >= 0 && delta_x >= 0 && delta_y < map->GetHeight() && delta_x < map->GetWidth()
 				&& algorithm_.tile_distace_.at(delta_x).at(delta_y) == current_distance)
 			{
-				current_tile_position.y = delta_y;			// Текущие координаты - координаты ячейки, помечена current_distance
-				current_tile_position.x = delta_x;			// Переходим в ячейку, которая на 1 ближе к старту
+				current_tile_position.y = delta_y;			// The current coordinates are the coordinates of the tile, labeled current_distance
+				current_tile_position.x = delta_x;			// Go to the tile, which is 1 closer to the start
 				break;
 			}
 		}
 	}
 
-	// Сохраняем координаты начальной ячейки в путь
+	// We store the coordinates of the start tile in the path
 	algorithm_.path_map_.push_back(map->GetStart());
 
 }
@@ -155,10 +155,10 @@ void LeeAlgorithm::ResetPathMap()
 void LeeAlgorithm::SetDirectionCount(const short number_of_directions)
 {
 
-	// Количество напрвлений
+	// Number of directions
 	number_of_directions_ = number_of_directions;
 
-	// Cмещения, соответствующие соседним ячейкам, в n направлений
+	// Places corresponding to neighboring tiles in n directions
 	if (algorithm_.GetNumberOfDirections() == 4)
 	{
 		offset_by_x_ = { 1, 0, -1, 0 };
