0a8cc7aecb86a6a4ff0f5b73df216bb9e1345fe0
HotBlueStreak <HotBlueStreak@mail.ru>
2017-12-19 20:55:19 +0300

    Revert "Lee algorithm"
    This reverts commit b710d8b4eea7fba9d2b43d2f2fe63ea44874aa20.

--- Sources/Algorithms/LeeAlgorithm.h
+++ Sources/Algorithms/LeeAlgorithm.h
@@ -3,22 +3,28 @@
 #include "../Map/Map.h"
 #include "../Map/Tile.h"
 
+struct Node 
+{
+	short x;
+	short y;
+	short distance;
+};
+
 class LeeAlgorithm
 {
 public:
-	static void FindPath(sf::Vector2f start_position, sf::Vector2f end_position, short number_of_directions);
+	static void FindPath(sf::Vector2f start_position, sf::Vector2f end_position, unsigned char number_of_directions);
 	static void SetBackTrace();
 	static void ShowPath();
 	static void ShowCheckedTiles();
-	static void ResetPathMap();
+	static void ResetPath();
 
 	static LeeAlgorithm* GetAlgorithm();
-	
-	void SetDirectionCount(const short number_of_directions);
-	void SetPathLength(const short path_length);
-	
+
+	void SetNumberOfDirections(unsigned char number_of_directions);
 	unsigned char GetNumberOfDirections();
-	short GetPathLength();
+
+	void SetDirectionCount(unsigned char number_of_directions);
 
 private:
 	LeeAlgorithm();
@@ -26,14 +32,15 @@ private:
 
 	static LeeAlgorithm algorithm_;
 
-	short number_of_directions_;
+	unsigned char number_of_directions_;
 	short path_length_;
 
-	std::vector<sf::Vector2f> path_map_;
+	std::vector<sf::Vector2f> path_map;
 	std::vector<std::vector<int>> tile_distace_;
-	std::vector<std::vector<bool>> checked_tile_;
+	//std::vector<std::vector<bool>> closed_set;
+	//std::vector<std::vector<bool>> closed_set;
 
-	std::vector<short> offset_by_x_;
-	std::vector<short> offset_by_y_;
+	std::vector<short> tile_offset_by_x_;
+	std::vector<short> tile_offset_by_y_;
 };
 
